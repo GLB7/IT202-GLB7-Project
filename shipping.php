3d62858713e97e6ce2d3c2ec7804d8ce759cd1b7
GLB7 <124634022+GLB7@users.noreply.github.com>
2024-02-14 15:24:30 -0500

    2/14/2024
    Still need to style it better and make sure all criteria for phase 1 is met.

--- shipping.php
+++ shipping.php
@@ -1,51 +1,49 @@
 <html>
     <head>
         <title>The Bibliophile's Commune Shipping</title>
-        <link rel="icon" href="store_logo.png" type="image/png"/>
+        <link rel="icon" href="images/store_logo.png" type="image/png"/>
         <link rel="stylesheet" href="shipping.css"/>
     </head>
     <body>
         <?php include ('header.php');?>
         <main>
-        <form action="#" method="post" id="packageForm">
-            <label for="toAddresses">First & Last Name:</label>
-            <input type="text" id="toAddresses" name="var" required>
-            <br>
+        <?php if (!empty($error_message)) { ?><p style="color: red; margin:0 25%; padding: 10px;"><?php echo htmlspecialchars($error_message);?></p><?php } ?>
+        <!-- anything on this form can be changed with inspect source so validate with php -->
+        <form action="shipping_label.php" method="post" id="packageform">
+            <label for="fname">First Name:</label>
+            <input type="text" id="fname" name="fname" placeholder="e.g., John" required><br>
 
-            <label for="toAddresses">Street Address:</label>
-            <input type="text" id="toAddresses" name="var" required>
-            <br>
+            <label for="lname">Last Name:</label>
+            <input type="text" id="lname" name="lname" placeholder="e.g., Doe" required><br>
 
-            <label for="toAddresses">City:</label>
-            <input type="text" id="toAddresses" name="var" required>
-            <br>
+            <label for="toaddress">Street Address:</label>
+            <input type="text" id="toaddress" name="toaddress" placeholder="e.g., 123 Main Street" required><br>
 
-            <label for="toAddresses">State:</label>
-            <input type="text" id="toAddresses" name="var" required>
-            <br>
+            <label for="city">City:</label>
+            <input type="text" id="city" name="city" placeholder="e.g., Newark" required><br>
 
-            <label for="toAddresses">Zip Code:</label>
-            <input type="text" id="toAddresses" name="var" required>
-            <br>
+            <label for="state">State:</label>
+            <input type="text" id="state" name="state" placeholder="e.g., New Jersey or NJ" required><br>
 
-            <label for="shipDate">Ship Date:</label>
-            <input type="date" id="shipDate" name="shipDate" required>
-            <br>
+            <label for="zipcode">ZIP Code:</label>
+            <input type="number" id="zipcode" name="zipcode" placeholder="e.g., 07105 or 07105-1234" required><br>
 
-            <label for="orderNumber">Order Number:</label>
-            <input type="text" id="orderNumber" name="orderNumber" required>
-            <br>
+            <label for="shipdate">Ship Date:</label>
+            <input type="date" id="shipdate" name="shipdate" required><br>
 
-            <label for="dimensions">Dimensions (Length x Width x Height):</label>
-            <input type="text" id="dimensions" name="dimensions" placeholder="e.g., 10x5x8" required>
-            <br>
+            <label for="ordernumber">Order Number:</label>
+            <input type="number" id="ordernumber" name="ordernumber" placeholder="e.g., 5" required><br>
 
-            <label for="declaredValue">Total Declared Value:</label>
-            <input type="text" id="declaredValue" name="declaredValue" placeholder="e.g., $100" required>
-            <br>
+            <label for="dimensions">Dimensions in inches (Length x Width x Height):</label>
+            <input type="text" id="dimensions" name="dimensions" placeholder="e.g., 10x5x7" required><br>
 
-            <button type="submit">Submit</button>
-            <br>
+            <label for="weight">Weight in pounds:</label>
+            <input type="number" id="dimensions" name="weight" placeholder="e.g., 50" required><br>
+
+            <label for="value">Total Declared Value:</label>
+            <input type="number" id="value" name="value" min="0" step="0.01" placeholder="e.g., 55.25" required><br>
+
+            <button type="submit">Get Shipping Label</button><br>
         </form>
         </main>   
     </body>
---
+++ shipping_label.php
@@ -0,0 +1,90 @@
+<?php
+   if ($_SERVER["REQUEST_METHOD"] == "POST"){ 
+    $error_message = '';
+    $states = array("AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL", "GA","HI","ID","IL","IN","IA","KS","KY","LA","ME", "MD","MA","MI","MN","MS","MO","MT","NE","NV","NH", "NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI", "SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY");
+    $state_names = array("Alabama","Alaska","Arizona","Arkansas","California", "Colorado","Connecticut","Delaware","District Of Columbia", "Florida","Georgia","Hawaii","Idaho","Illinois","Indiana", "Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland", "Massachusetts",">Michigan","Minnesota","Mississippi","Missouri", "Montana","Nebraska","Nevada","New Hampshire","New Jersey", "New Mexico","New York","North Carolina","North Dakota","Ohio", "Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina", "South Dakota","Tennessee","Texas","Utah","Vermont","Virginia", "Washington","West Virginia","Wisconsin","Wyoming");
+    
+    $fname = filter_input(INPUT_POST, 'fname', FILTER_SANITIZE_STRING);
+    $lname = filter_input(INPUT_POST, 'lname', FILTER_SANITIZE_STRING);
+    $toaddress = filter_input(INPUT_POST, 'toaddress', FILTER_SANITIZE_STRING);
+    $city = filter_input(INPUT_POST, 'city', FILTER_SANITIZE_STRING);
+    $state = filter_input(INPUT_POST, 'state', FILTER_SANITIZE_STRING);
+    $zipcode = filter_input(INPUT_POST, 'zipcode', FILTER_SANITIZE_NUMBER_INT);
+    $shipdate = filter_input(INPUT_POST, 'shipdate', FILTER_SANITIZE_STRING);
+    $ordernumber = filter_input(INPUT_POST, 'ordernumber', FILTER_SANITIZE_NUMBER_INT);
+    $dimensions = filter_input(INPUT_POST, 'dimensions', FILTER_SANITIZE_STRING);
+    $weight = filter_input(INPUT_POST, 'weight', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    $value = filter_input(INPUT_POST, 'value', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+
+    if (!in_array($state, $states) && !in_array($state, $state_names)) {
+        $error_message = "Please enter a valid U.S. state.";
+    }
+
+    if (!preg_match('/^\d{5}(-\d{4})?$/', $zipcode)) {
+        $error_message = "Please enter a valid ZIP code.";
+    }
+
+    $dimensionsArray = explode('x', $dimensions); # Splits string by delimiter
+    foreach ($dimensionsArray as $dimension) {
+        if ($dimension > 36) {
+            $error_message = "Each dimension must not be greater than 36 inches.";
+            break;
+        }
+    }
+
+    if ($value > 1000) {
+        // Check if the value is less than 1000
+        $error_message = "The total declared value of the package must not exceed $1,000.";
+    }
+
+    if($error_message != '') {
+        include('shipping.php');
+        exit();
+    }
+
+    // TODO apply formatting
+    $dimensions = $dimensions . ' inches';
+    $weight = $weight . ' lbs';
+    $value = '$' . number_format($value, 2);
+
+   }else{
+        header("Location: shipping.php");
+   }
+
+?>
+<html>
+    <head>
+        <title>The Bibliophile's Commune Shipping</title>
+        <link rel="icon" href="images/store_logo.png" type="image/png"/>
+        <link rel="stylesheet" href="shipping_label.css"/>
+    </head>
+    <body>
+        <?php include ('header.php');?>
+        <main>
+        <div id="shipping-label">Shipping Label</div>
+        <div id="shipping-info">
+            <p>US POSTAGE</p>
+            <p><span><?php echo $shipdate; ?></span></p>
+            <p>Order Number: <span><?php echo $ordernumber; ?></span></p>
+            <p>Dimensions: <span><?php echo $dimensions; ?></span></p>
+            <p>Weight: <span><?php echo $weight; ?></span></p>
+            <p>Value: <span><?php echo $value; ?></span></p>
+        </div>
+        <div id="usps-mail"> USPS PRIORITY MAIL</div>
+        <div id="shipping-info">
+            <p>The Bibliophile's Commune</p>
+            <p>Clifton Commons 395 Route 3 East</p>
+            <p>Clifton, NJ 07014</p>
+        </div>
+        <div id="to-address">
+            <p><span><?php echo $toaddress; ?></span></p>
+            <p><span><?php echo $city . ', ' . $state . ' ' . $zipcode; ?></span></p>
+        </div>
+        <div id="tracking-number">
+            <p>USPS TRACKING #</p>
+            <img src="images/trackingnum.png" alt="trackingnum.png" width="350"/>
+        </div>
+        </main>   
+    </body>
+    <?php include ('footer.php');?>
+</html>
